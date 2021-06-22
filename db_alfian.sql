ecd5c8c11867df33c1f191259073e9bca7364086
Hidayat <hidayat@Hidayats-MacBook-Pro.local>
2021-06-22 15:59:57 +0700

    added databse

--- db_alfian.sql
+++ db_alfian.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80023
  File Encoding         : 65001
 
- Date: 08/06/2021 14:14:32
+ Date: 22/06/2021 15:59:33
 */
 
 SET NAMES utf8mb4;
@@ -27,14 +27,16 @@ CREATE TABLE `alternatif` (
   `id_kriteria` int DEFAULT NULL,
   `nilai` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- ----------------------------
 -- Records of alternatif
 -- ----------------------------
 BEGIN;
-INSERT INTO `alternatif` VALUES (1, 1, 2, '10');
-INSERT INTO `alternatif` VALUES (2, 1, 1, '20');
+INSERT INTO `alternatif` VALUES (2, 1, 2, '22');
+INSERT INTO `alternatif` VALUES (3, 1, 3, '20');
+INSERT INTO `alternatif` VALUES (4, 1, 1, '51');
+INSERT INTO `alternatif` VALUES (5, 2, 2, '2');
 COMMIT;
 
 -- ----------------------------
@@ -43,19 +45,20 @@ COMMIT;
 DROP TABLE IF EXISTS `kriteria`;
 CREATE TABLE `kriteria` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `nama_kriteria` varchar(255) DEFAULT NULL,
+  `nama_kriteria` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- ----------------------------
 -- Records of kriteria
 -- ----------------------------
 BEGIN;
 INSERT INTO `kriteria` VALUES (1, 'Kehadiran');
-INSERT INTO `kriteria` VALUES (2, 'keterlambatan');
-INSERT INTO `kriteria` VALUES (3, 'kepribadian');
-INSERT INTO `kriteria` VALUES (4, 'pakaian');
-INSERT INTO `kriteria` VALUES (5, 'ketertiban');
+INSERT INTO `kriteria` VALUES (2, 'Keterlambatan');
+INSERT INTO `kriteria` VALUES (3, 'Merokok');
+INSERT INTO `kriteria` VALUES (4, 'Perkelahian');
+INSERT INTO `kriteria` VALUES (5, 'Membawa Senjata');
+INSERT INTO `kriteria` VALUES (6, 'Obat Terlarang / Miras');
 COMMIT;
 
 -- ----------------------------
@@ -65,15 +68,16 @@ DROP TABLE IF EXISTS `siswa`;
 CREATE TABLE `siswa` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nama_siswa` varchar(255) DEFAULT NULL,
+  `no_tlp` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- ----------------------------
 -- Records of siswa
 -- ----------------------------
 BEGIN;
-INSERT INTO `siswa` VALUES (1, 'Dayatjeh');
-INSERT INTO `siswa` VALUES (2, 'Nur');
+INSERT INTO `siswa` VALUES (1, 'Dayatjeh', '628347877');
+INSERT INTO `siswa` VALUES (2, 'Nur', '62863972346');
 COMMIT;
 
 -- ----------------------------
@@ -84,12 +88,12 @@ CREATE TABLE `user` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nama` varchar(255) DEFAULT NULL,
   `username` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT '$2y$10$tlmiJXxRHfOXMNkfXD.u9.ftYyx5i2aTH4viJJ0oDlLVNy9Nj75n6',
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT '$2y$10$tlmiJXxRHfOXMNkfXD.u9.ftYyx5i2aTH4viJJ0oDlLVNy9Nj75n6',
   `role` int DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `status` int DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- ----------------------------
 -- Records of user
